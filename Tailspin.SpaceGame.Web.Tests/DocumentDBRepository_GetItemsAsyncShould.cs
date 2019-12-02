244be147761d59dc877ad4aacb8537f6a8ca8023
bj3mas <bjorn-marcus-motzfeldt.auberg@capgemini.com>
2019-12-02 11:02:31 +0100

    fix bug

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -49,8 +50,9 @@ namespace Tests
 
             // Verify that each score's game region matches the provided game region.
             Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
+        }
 
-            [TestCase(0, ExpectedResult = 0)]
+        [TestCase(0, ExpectedResult = 0)]
         [TestCase(1, ExpectedResult = 1)]
         [TestCase(10, ExpectedResult = 10)]
         public int ReturnRequestedCount(int count)
